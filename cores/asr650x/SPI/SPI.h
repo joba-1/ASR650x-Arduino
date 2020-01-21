50e160dce0956b7f3def2331ef90705c7360648e
Joachim Banzhaf <Joachim.Banzhaf@gmail.com>
2020-01-21 19:07:02 +0100

    add fake SPI settings for libs using this

--- cores/asr650x/SPI/SPI.h
+++ cores/asr650x/SPI/SPI.h
@@ -25,6 +25,19 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+// joba: SPI modes grabbed from a newer version of esp8266 header. Needed for compile, not tested
+const uint8_t SPI_MODE0 = 0x00; ///<  CPOL: 0  CPHA: 0
+const uint8_t SPI_MODE1 = 0x01; ///<  CPOL: 0  CPHA: 1
+const uint8_t SPI_MODE2 = 0x10; ///<  CPOL: 1  CPHA: 0
+const uint8_t SPI_MODE3 = 0x11; ///<  CPOL: 1  CPHA: 1
+
+// joba fake class. CubeCell does not support settings (yet?)
+class SPISettings
+{
+public:
+  SPISettings(uint32_t , uint8_t , uint8_t ) {}
+  SPISettings() {}
+};
 
 class SPIClass
 {
@@ -37,6 +50,7 @@ public:
     void begin(uint8_t ss=-1);
     void end();
     void beginTransaction(void);
+    void beginTransaction(SPISettings ) { beginTransaction(); } // joba: fake settings for arduino libs using them
     void endTransaction(void);
     void transfer(uint8_t * data, uint32_t size);
     uint32_t transfer(uint32_t data);
